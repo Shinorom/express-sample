394c421e09e3191a262646c4f01eee03ceb630c5
Kamolwan Sahapornaudomkarn <kamolwan.june@gmail.com>
2019-03-31 10:37:50 +0700

    backent1

--- controller/testController.js
+++ controller/testController.js
@@ -9,19 +9,48 @@ let responseObject = {
 }
 
 router.get("/getData",  (req, res) => {
-    let result = req.query.key1 * req.query.key2;
-    res.send({ result: result })
+    let result = Number(req.query.key1) + Number( req.query.key2);
+    res.send({ sum : result })
+});
+
+router.get("/getTriangleArea",  (req, res) => {
+    let result = Number((1/2)*req.query.base*req.query.height);
+    res.send({TriangleArea : result })
+});
+
+router.get("/getBMI",  (req, res) => {
+    let result = Number(req.query.weight/((req.query.height/100)*(req.query.height/100)));
+    let msg="";
+
+    if(result>25){
+        msg="Fat";
+    }else if(result==25){
+        msg="Normal";
+    }else if(result<25){
+        msg="Thin";
+    }
+    res.send(
+        {
+            BMI : result.toFixed(2),
+            status : msg   
+            })
 });
 /*
 Request
 * */
 router.post("/postData",async  (req, res) => {
-    if(!req.body.key5){
-        responseObject.msg = "Bad request";
-    }else{
+    
+    responseObject = {
+        msg: "",
+        result: ""
+    }
+
+     if(!req.body.key){
+         responseObject.msg = "Bad request";
+     }else{
         responseObject.msg = "success";
         responseObject.result = req.body.key * 5;
-    }
+     }
     res.send(responseObject);
 });
 
